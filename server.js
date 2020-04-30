19fd8bf271e5f2575563bc20ea20979d1275ed6f
matko031 <matkomatic59@gmail.com>
2020-04-30 14:54:28 +0200

    added database

--- server.js
+++ server.js
@@ -1,34 +1,38 @@
+const uniqid = require('uniqid');
 const session = require("express-session");
 const express= require("express");
 const bodyParser = require("body-parser");
 const fs = require('fs');
 const passport = require('passport');
-
-
 const initializePassport = require('./passport-config');
 initializePassport(passport);
+const mysql = require('mysql');
+
+/* localhost
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'matko',
+    password: 'matkoisthebest',
+    database: 'anonymous_voting'
+});
+
+*/
 
 
-
-const getElectorate = (next) => {
-    const codes_file = fs.readFile('codes', 'utf8', (err, data) => {
-        const electorate = {};
-        const lines = data.split('\n').filter( (elem) => elem != '' );
-        lines.forEach( (line) => {  
-            temp = line.split(',');
-            electorate[temp[1]] = temp[0];
-        })
-        next(electorate);
-    });
-};
+const db = mysql.createConnection({
+    username: 'b403519124d5e3',
+    password: '2977cb03',
+    host: 'eu-cdbr-west-03.cleardb.net',
+    database: 'heroku_e2ec6684540f209'
+});
 
 
-let electorate = {};
-getElectorate( (elect) => { electorate = elect} );
-
-
-	
-
+db.connect( (err) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log('MySql Connected...');
+});
 const app = express();
 
 //Set view engine to ejs
@@ -46,83 +50,57 @@ app.use(session({
 	secret: 'matkoisthebest',
 	resave: true,
 	saveUninitialized: true,
-}))
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 
-currentVoting = "president";
+currentVoting = "14";
 
-
-
-
-const getAllPositions = (next) => {
-    fs.readdir('candidates', (err, files) => {
-        if(err) {
-            next([]);
-        } else {
-            next(files);
-        }
+const getUsers= (next) => {
+    const sql = "SELECT username, code FROM user";
+    db.query(sql, (err, res) => {
+        if (err) console.log('getUsers', err);
+        else next(res);
     });
 };
 
 
-const getCandidatesForPosition = (position, next) => {
-    let itemsProcessed = 0;
-    fs.readFile('candidates/'+position, 'utf8', (err, data) => {
-        cands = [];
-        if (!err){
-            cands = data.split('\n').filter( (elem) => elem != '');
-        }
-        next(cands);
-    });
-}
-
-
-
-const getAllVotedForPosition = (position, next) => {
-
-    const votedFile = 'voted/' + position;
-    fs.readFile(votedFile, 'utf8', (err, data) => {
-        if (err) {
-            if (err.code === "ENOENT"){
-                error = "";
-                fs.appendFile(votedFile, '', (err) => {
-                    next([]);
-                });
-            }
-        } else {
-            next(data.split('\n').filter( elem => elem != ''));
-        }
+const getQuestions = (next) => {
+    const sql = "SELECT question_id, question_text FROM question";
+    db.query(sql, (err, res) => {
+        if (err) console.log('getQuestions', err);
+        else next(res);
     });
 };
 
 
 
-const getAllVotesForPosition = (position, nextStep) => {
-
-    const votesFile = 'votes/' + position;
-    fs.readFile(votesFile, 'utf8', (err, data) => {
-        if (err) {
-            error = err;
-            if (err.code === "ENOENT"){
-                error = "";
-                fs.appendFile(votesFile, '', (err) => {
-                    nextStep([]);
-                });
-            }
-        } else {
-            nextStep(data.split('\n').filter( elem => elem != ''));
-        }
+const getQuestion = (question_id, next) => {
+    const sql = `SELECT question_text FROM question WHERE question_id=${question_id}`;
+    db.query(sql, (err, res) => {
+        if (err) console.log('getQuestion', err);
+        else next(res[0].question_text);
     });
 };
 
+
+const getAnswers = (question_id, next) => {
+    const sql = `SELECT answer_text, answer_id FROM answer WHERE question_id = ${question_id}`;
+    db.query(sql, (err, res) => {
+        if (err) console.log('getAnswer', err);
+        else next(res);
+    });
+};
 
 
 
 
 app.get("/", (req, res) => { 
-    getCandidatesForPosition (currentVoting, (candidates) => {
-        res.render("index", {name: currentVoting, candidates : candidates});
+    getQuestion( currentVoting, (q) =>{
+        getAnswers(currentVoting, (answers) => {
+            res.render("index", {question: q, question_id: currentVoting, answers : answers});
+        });
+
     });
 });
 
@@ -131,36 +109,42 @@ app.get("/", (req, res) => {
 app.post("/vote", (req, res) => {
 	const code = req.body.code;
 	const vote = req.body.vote;
-	const position = req.body.position;
-    const person = electorate[code];
+    const question_id = req.body.question_id;
 
-    const votedFile = 'voted/' + currentVoting;
-    const votesFile = 'votes/' + currentVoting;
-    
+
     error = "";
-    if (position !== currentVoting) {
+    if ( question_id !== currentVoting){
         error = "The voting position has changed after you have opened the voting page. Please go back to the homepage and vote again";
         res.render("done", {err: error});
-    } else if (! electorate.hasOwnProperty(code)){
-        error = "The code you have typed does not exist";
-        res.render("done", {err: error});
     } else {
-
-        getAllVotedForPosition( currentVoting, (voted) => {
-            if ( ! voted.includes(person) ){
-                voted.push(person);
-                shuffle(voted);
-                fs.writeFile(votedFile, voted.join('\n'), (err) => error = err);
-
-                getAllVotesForPosition( currentVoting, (votes) =>{
-                    votes.push(vote);
-                    shuffle(votes);
-                    fs.writeFile(votesFile, votes.join('\n'), (err) => error = err)
-                });
-            } else {
-                error = 'You have already voted';
+        sql1 = `SELECT user_id FROM user WHERE code='${code}'`;
+        db.query(sql1, (err, result) => {
+            if (err) console.log(err);
+            else{
+                if(result.length === 0){
+                    error = "The code you have typed does not exist";
+                    res.render("done", {err: error});
+                } else{
+                    const user_id = result[0].user_id; 
+                    sql2 = `SELECT user_id FROM voted WHERE user_id=${user_id} AND question_id=${currentVoting}`;
+                    db.query(sql2, (err, result) => {
+                        if(err) console.log(err);
+                        if(result.length === 0){
+                           sql3 = `INSERT INTO voted VALUES(${user_id}, ${currentVoting})`;
+                            db.query(sql3, (err,result) => {
+                               sql4 = `INSERT INTO vote (answer_id) VALUES(${vote})`;
+                                db.query(sql4, (err, result) =>{
+                                    if(err) console.log(err); 
+                                    res.render("done", {err: error});
+                                });
+                            });
+                        } else{
+                            error = "You have already votes for this question"; 
+                            res.render("done", {err: error});
+                        } 
+                    });
+                }
             }
-            res.render("done", {err: error});
         });
     }
 });
@@ -168,79 +152,98 @@ app.post("/vote", (req, res) => {
 
 
 app.get("/adminlogin", (req, res) => { 
-	res.render("login")
+	res.render("login");
 });
 
 
 app.post("/adminlogin", passport.authenticate('local', {
 	successRedirect: '/dashboard',
 	failureRedirect: 'adminlogin'
-}))
+}));
 
 
 
 app.get("/dashboard", (req, res) => {
-	if (req.user === "matko"){
+//	if (req.user === "matko"){
+    getUsers( (users) => {
+        getQuestions ( (questions) =>{
+            res.render("dashboard", { questions : questions, users: users});
+        }) ;
+    });
 
-        getAllPositions( (allPositions) => {
-            res.render("dashboard", {candidates:allPositions});
-        });
-	} else{
-		res.redirect("/");
-	}
+//	} else{
+//		res.redirect("/");
+//	}
 
-})
+});
 
 
-app.post("/addCandidate", (req, res) => {
-	if (req.user === "matko"){
-        Object.keys(req.body).forEach( (position) => {
-            fs.appendFile('candidates/'+position, req.body[position]+'\n', (err) => {if(err) console.log(err);} );
-        })
-        res.redirect("/");
-    }
+app.post("/addUser", (req, res) => {
+//	if (req.user === "matko"){
+    const username = req.body.username;
+    const code = uniqid();
+    const sql = `INSERT INTO user (username, code) VALUES ('${username}', '${code}')`;
+    db.query(sql, (err, result) => {
+        res.redirect('/dashboard');
+    });
+ //   }
 });
 
 
 
+
+app.post("/addQuestion", (req, res) => {
+//	if (req.user === "matko"){
+    const question = req.body.question;
+    const sql = `INSERT INTO question (question_text) VALUES ('${question}')`;
+    db.query(sql, (err, result1) => {
+
+        const sql2 = `SELECT question_id FROM question WHERE question_text = '${question}'`;
+        db.query(sql2, (err, result2) => {
+            
+            const q_id = result2[0].question_id;
+
+            let processed = 0;
+            const answers = ['accept', 'reject', 'abstain', 'withdraw'];
+            for (let i=0; i<4; i++){
+                let sql3 = `INSERT INTO answer (answer_text, question_id) VALUES ('${answers[i]}', ${q_id})`;
+                db.query(sql3, (err, result2) => {
+                    processed++;
+                    if (err) {console.log(err);}
+                    if (processed  == 4){
+                        res.redirect('/dashboard');                    
+                }
+                });
+            }
+        });
+    });
+
+ //   }
+});
+
 app.post("/changeCurrentVoting", (req, res) => {
-    if (req.user === "matko"){
+//    if (req.user === "matko"){
         if('currentVoting' in req.body){
             currentVoting = req.body.currentVoting;
         }
-    }
+//    }
     res.redirect("/");
 });
 
 app.get("/results", (req, res) => {
-    console.log(electorate);
-    results  = [];
-    getAllPositions((positions) => {
-        positionsProcessed = 0;
-        positions.forEach( (position, index, array) => {
-            getAllVotedForPosition(position, (voted) => {
-                getAllVotesForPosition(position, (votes) => {
-                    positionsProcessed++;
-                    
-                    votes_result = {};
-                    votes.forEach( (vote) => {
-                        if (! (vote in votes_result)){
-                            votes_result[vote] = 1; 
-                        } else {
-                            votes_result[vote] += 1; 
-                        } 
-                    });
-                    
-                    votes_numbers = Object.keys(votes_result).map( (key) => { return {name: key, number:votes_result[key]} })
-                    let toPush = {position: position, voted: voted, votes:votes_numbers} ;
-                    results.push(toPush);
-                     
-                    if (positionsProcessed === array.length){
-                        res.render("results", {results: results}); 
-                    }
-                }); 
-            }) 
+    const sql = "SELECT question_text, answer_text, COUNT(*) AS votes FROM vote INNER JOIN answer ON vote.answer_id=answer.answer_id INNER JOIN question ON answer.question_id = question.question_id GROUP BY vote.answer_id";
+    db.query(sql, (err, result) => {
+        if(err) console.log(err); 
+        console.log(result);
+        let processed = 0;
+        voting_results = {};
+        result.forEach( elem =>{
+            if (! voting_results.hasOwnProperty(elem.question_text)){
+                voting_results[elem.question_text] = {}; 
+            }
+            voting_results[elem.question_text][elem.answer_text] = elem.votes;
         });
+        res.render('results', {results: voting_results});
     });
 });
 
@@ -260,5 +263,5 @@ function shuffle(array) {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`);
 });
