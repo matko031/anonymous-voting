f2467b62cba07e776d99d5c016b4024b9d74283e
matko031 <38529583+matko031@users.noreply.github.com>
2022-11-28 14:17:32 +0100

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-const db_config = {
-    user: 'b403519124d5e3',
-    password: '2976cb03',
-    host: 'eu-cdbr-west-03.cleardb.net',
-    database: 'heroku_e2ec6684540f209',
-    connectionLimit : 9 
-};
-
-module.exports = db_config;
